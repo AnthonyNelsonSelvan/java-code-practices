d088419be7cd21b19c17d8cc1d54debd121fa973
AnthonyNelsonSelvan <anthony.nelson.selvan@gmail.com>
2024-10-23 16:27:28 +0530

    Create map.java

---
+++ map.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class map{
+  public static void main[String[] args]{
+    Map<Integer, String> friends = new HashMap<>();
+    friends.put(1,"nelson");
+    friends.put(2,"sri");
+    friends.put(3,"kanna");
+    friends.put(4,"sudar");
+    friends.put(5,"vicky");
+    friends.put(6,"suvin");
+    System.out.println(friends);
+  }
+}
